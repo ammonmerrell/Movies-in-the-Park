560c6c7e69f1f1dfeb32fb5e1d278ad66418c85d
ammonmerrell <amerrell@byupathway.edu>
2025-11-28 10:51:03 -0700

    filled out product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,10 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
+const dataSource = new ExternalServices("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productDetails = new ProductDetails(productId, dataSource);
+productDetails.init();
