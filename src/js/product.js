6cbd6386fd406b0fe207ac4852b23a6881a7cd2d
ammonmerrell <amerrell@byupathway.edu>
2025-11-29 07:39:28 -0700

    commented out import utils from product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
-import { getParam, loadHeaderFooter } from "./utils.mjs";
+// import { getParam, loadHeaderFooter } from "./utils.mjs";
 import ExternalServices from "./ExternalServices.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+// import ProductDetails from "./ProductDetails.mjs";
 loadHeaderFooter();
 
 const dataSource = new ExternalServices("tents");
